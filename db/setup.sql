cae4e08b0c6a72bd4280606236fdf595f65a8306
David Perez <dperez19279@columbushs.com>
2017-12-05 23:32:58 -0500

    Fix setup.sql

--- db/setup.sql
+++ db/setup.sql
@@ -1,33 +1,34 @@
 -- Delete old tables
---DROP TABLE IF EXISTS users, books, people, patrons, checked_out_books CASCADE;
---DROP TYPE IF EXISTS role CASCADE;
+DROP TABLE IF EXISTS users, books, people, patrons, checked_out_books CASCADE;
+DROP TYPE IF EXISTS role CASCADE;
 
--- Create ENUM's and TABLE's
-CREATE OR REPLACE TYPE role AS ENUM ('teacher','student');
+-- Create ENUMs
+CREATE TYPE role AS ENUM ('teacher','student');
 -- CREATE TYPE ___ AS ENUM ('dewey_decimal','author','title','subject'); -- or a opt in checkbox
 
-CREATE OR REPLACE TABLE users (
+-- Create tables
+CREATE TABLE users (
     cookie text PRIMARY KEY,
     library_name text,
     teacher_checkout_length integer,
     student_checkout_length integer
 );
 
-CREATE OR REPLACE TABLE books (
+CREATE TABLE books (
     id serial PRIMARY KEY,
     name text,
     quantity integer CHECK (quantity > 0), -- will be removed
     user_cookie text REFERENCES users ON DELETE CASCADE
 );
 
-CREATE OR REPLACE TABLE patrons (
+CREATE TABLE patrons (
     id serial PRIMARY KEY,
     name text,
     role role,
     user_cookie text REFERENCES users ON DELETE CASCADE
 );
 
-CREATE OR REPLACE TABLE book_copies ( -- will be renamed to something that has isbn or similar meaning
+CREATE TABLE book_copies ( -- will be renamed to something that has isbn or similar meaning
     id serial PRIMARY KEY,
     book serial REFERENCES books ON DELETE CASCADE,
     patron serial REFERENCES patrons ON DELETE CASCADE, -- change to SET NULL so that table will not delete data on
