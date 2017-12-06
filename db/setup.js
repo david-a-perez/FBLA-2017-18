28dc9cf390a56be7a14ef8a05735f592898a73d5
David Perez <dperez19279@columbushs.com>
2017-12-05 23:21:50 -0500

    Improve setup.sql

--- db/setup.js
+++ db/setup.js
@@ -1,5 +1,5 @@
 const db = require('./query');
 const fs = require('fs');
-
+// Read and execute the setup.sql file
 const contents = fs.readFileSync('setup.sql', 'utf8');
 db.query(contents);
--- db/setup.sql
+++ db/setup.sql
@@ -1,31 +1,33 @@
-DROP TABLE IF EXISTS users, books, people, patrons, checked_out_books CASCADE;
-DROP TYPE IF EXISTS role CASCADE;
+-- Delete old tables
+--DROP TABLE IF EXISTS users, books, people, patrons, checked_out_books CASCADE;
+--DROP TYPE IF EXISTS role CASCADE;
 
-CREATE TYPE role AS ENUM ('teacher','student');
+-- Create ENUM's and TABLE's
+CREATE OR REPLACE TYPE role AS ENUM ('teacher','student');
 -- CREATE TYPE ___ AS ENUM ('dewey_decimal','author','title','subject'); -- or a opt in checkbox
 
-CREATE TABLE users (
+CREATE OR REPLACE TABLE users (
     cookie text PRIMARY KEY,
     library_name text,
     teacher_checkout_length integer,
     student_checkout_length integer
 );
 
-CREATE TABLE books (
+CREATE OR REPLACE TABLE books (
     id serial PRIMARY KEY,
     name text,
     quantity integer CHECK (quantity > 0), -- will be removed
     user_cookie text REFERENCES users ON DELETE CASCADE
 );
 
-CREATE TABLE patrons (
+CREATE OR REPLACE TABLE patrons (
     id serial PRIMARY KEY,
     name text,
     role role,
     user_cookie text REFERENCES users ON DELETE CASCADE
 );
 
-CREATE TABLE checked_out_books ( -- will be renamed to something that has isbn or similar meaning
+CREATE OR REPLACE TABLE book_copies ( -- will be renamed to something that has isbn or similar meaning
     id serial PRIMARY KEY,
     book serial REFERENCES books ON DELETE CASCADE,
     patron serial REFERENCES patrons ON DELETE CASCADE, -- change to SET NULL so that table will not delete data on
