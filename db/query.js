3a5f416b6643c51bd466e0ef4ede936c1093f320
David Perez <dperez19279@columbushs.com>
2017-12-13 16:09:21 -0500

    Remove old comments

--- db/query.js
+++ db/query.js
@@ -1,9 +1,8 @@
 const pg = require('pg');
 const moment = require('moment');
+
 // Interpret date as a simple string
-//pg.types.setTypeParser(1082, str => new Date(str).toDateString());
 pg.types.setTypeParser(1082, str => moment(str, 'YYYY-MM-DD').format('ddd MMM DD YYYY'));
-//pg.types.setTypeParser(1082, str => console.log(str));
 
 const pool = new pg.Pool({
     connectionString: process.env.DATABASE_URL
